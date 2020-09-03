4a402cda1afbdb02b81f8d84de5ad24452f0614b
dickheadhex <dickheadhex@centos.serverx>
2020-09-03 06:09:49 +0530

    only for path /

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const fs = require('fs');
 
 
 const server = http.createServer((req,res) => {
-	res.writeHead(200, { "Content-Type": "text/html"});
-	res.write("request received:<br>");
-	res.write(req.method+"<br>");
-	res.write(req.url+"<br>");
-	res.write(JSON.stringify(req.headers)+"<br>");
-	res.end();
+	if(req.url === '/'){
+		res.writeHead(200, { "Content-Type": "text/html"});
+		res.write("request received:<br>");
+		res.write(req.method+"<br>");
+		res.write(req.url+"<br>");
+		res.write(JSON.stringify(req.headers)+"<br>");
+		res.end();
+	}
 });
 
 const PORT = process.env.PORT || 8080;
